96f84a0b0f53bd131792ba2ada9aa2e5625d7496
nickspeelman <75999598+nickspeelman@users.noreply.github.com>
2024-11-15 11:25:21 -0500

    changes

--- google_sheets_manager.py
+++ google_sheets_manager.py
@@ -24,15 +24,16 @@ def update_game_state(game_state):
     logger.info("Updated game state")
     headers = {"Content-Type": "application/json"}
     data = json.dumps({"key": ACCESS_KEY, **game_state})
-    logger.info('Data sent to google sheets: ', data)
+    logger.info(f"Data sent to google sheets: {data}")
     response = requests.post(APPS_SCRIPT_URL, headers=headers, data=data)
 
     # 2. Check if update to Sheets was successful
     if response.status_code == 200:
         # 3. Sync `st.session_state.game_state` with the updated state
         st.session_state.game_state = game_state
-        logger.info("Game State: ", game_state)
+        logger.info(f"Game State: {game_state}")
         return True
     else:
         st.error("Failed to update Google Sheets.")
         return False
+#%%
